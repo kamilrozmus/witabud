852c19e4aec800104ebbe7954fc938434474af07
Kamil Rozmus <kamil.rozmus@digitalholding.pl>
2020-11-16 19:03:30 +0100

    main page started

---
+++ src/components/Header.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-app-bar
+    app
+    absolute
+    color="transparent"
+    dark
+    flat
+    class="header"
+  >
+    <div class="d-flex align-center">
+      <v-img
+        alt="logo"
+        class="shrink mr-2"
+        contain
+        src="@/assets/img/logo-black.png"
+        transition="scale-transition"
+      />
+    </div>
+
+    <v-spacer></v-spacer>
+
+    <nav>
+      <ul>
+        <li><a href="#">Strona główna</a></li>
+        <li><a href="#">O firmie</a></li>
+        <li><a href="#">Usługi</a></li>
+        <li><a href="#">Realizacje</a></li>
+        <li><a href="#">Kontakt</a></li>
+      </ul>
+    </nav>
+  </v-app-bar>
+</template>
+<style lang="scss" scoped>
+@import '@/assets/styles.scss';
+.header {
+  margin: auto;
+  width: 66%;
+}
+
+nav {
+  float: right;
+}
+
+nav ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+
+}
+
+nav li {
+  display: inline-block;
+  margin-left: 70px;
+  padding: 23px 0 12px 0;
+  position: relative;
+
+}
+
+nav a {
+  color: black;
+  text-decoration: none;
+  font-weight: 800;
+  font-family: 'LeagueSpartan', sans-serif;
+  text-transform: uppercase;
+  padding: 21px 10px;
+  font-size: 14px;
+
+  &:hover {
+    border-bottom: 5px solid $yellow-primary;
+    color: $yellow-primary;
+    margin-top: 15px;
+  }
+  &:focus {
+    outline: none;
+    box-shadow: none;
+  }
+}
+
+</style>
---
+++ src/components/MainPage.vue
@@ -0,0 +1,191 @@
+<template>
+  <div>
+    <v-carousel v-model="model" hide-delimiters height="880px">
+      <v-carousel-item
+        v-for="(item, i) in slides"
+        :key="i"
+        transition="fade-transition"
+        prev-icon="mdi-arrow-right"
+        next-icon="mdi-arrow-right"
+      >
+        <v-sheet>
+          <v-img :src="item.src" style="width: 100%;">
+            <div style="transform: translate(20%, 90%);">
+              <h1>
+                <div class="name">Witold Goj</div>
+                <div>Usługi remontowo</div>
+                <div>Wykonczeniowe</div>
+              </h1>
+              <button class="slider-btn service" dark large>Zobacz usługi</button>
+              <button class="slider-btn phone">Zadzwoń +48 639 859 748</button>
+            </div>
+            <div class="slider-footer">
+              <div class="title">Zobacz nasze usługi</div>
+              <button>
+                <v-icon
+                  large
+                  class="mt-3"
+                >
+                  mdi-chevron-down
+                </v-icon>
+              </button>
+            </div>
+          </v-img>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+
+    <div class="services">
+      <div v-for="(item, i) in servicesImg" :key="i" class="child">
+        <img :src="item.src" class="img-child">
+      </div>
+    </div>
+
+    <div class="reviews-panel">
+      <div class="childs">
+        <div class="child my-5">
+          <v-avatar class="mt-6">
+            <img
+              src="https://picsum.photos/200"
+              alt="avatar"
+            >
+          </v-avatar>
+          <div class="ml-5" style="max-width: 270px;">
+            <div>Joanna Kowalska</div>
+            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium odio illo voluptates repudiandae distinctio est</span>
+        </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+    data: () => ({
+      model: 0,
+      slides: [
+        {
+          src: require('@/assets/img/slide1.png')
+        },
+        {
+          src: require('@/assets/img/slide2.png')
+        }
+      ],
+      servicesImg: [
+        {
+          src: require('@/assets/img/painting.png'),
+        },
+        {
+          src: require('@/assets/img/cutting.png')
+        },
+        {
+          src: require('@/assets/img/floor.png')
+        },
+        {
+          src: require('@/assets/img/desk-floor.png')
+        }
+      ]
+    })
+  }
+</script>
+<style lang="scss" scoped>
+@import '@/assets/styles.scss';
+
+h1 {
+  font-size: 55px;
+  margin-left: 10px;
+  text-transform: uppercase;
+  .name {
+    font-size: 35px;
+    color: $dirty-white;
+  }
+}
+
+.slider-btn {
+  position: relative;
+  border: transparent;
+  margin: 10px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 16px;
+  opacity: 100%;
+  line-height: 145%;
+  font-weight: 800;
+  font-family: 'LeagueSpartan', sans-serif;
+  text-transform: uppercase;
+  height: 50px;
+  cursor: pointer;
+  &:focus {
+    outline: none;
+    box-shadow: none;
+  }
+  &.service {
+    color: $black-primary;
+    background: $yellow-primary;
+    width: 200px;
+    &:hover {
+      color: $yellow-primary;
+      background: $black-primary;
+      opacity: 75%;
+    }
+  }
+  &.phone {
+    color: $yellow-primary;
+    background: $black-primary;
+    opacity: 75%;
+    width: 300px;
+    &:hover {
+      color: $black-primary;
+      background: $yellow-primary;
+      opacity: 100%;
+    }
+  }
+}
+
+.services {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: flex-start;
+}
+
+.child {
+  flex: 1 1 25%;
+}
+
+.img-child {
+  width: 100%;
+  max-height: 300px;
+}
+
+.reviews-panel {
+  padding-top: 0;
+  display: flex;
+  justify-content: center;
+  background: $dark-bg;
+
+  .childs {
+    display: flex;
+    justify-content: center;
+
+    .child {
+      display: flex;
+      border: 1px solid white;
+    }
+  }
+}
+
+.slider-footer {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+  transform: translateY(5%);
+  .title {
+    font-size: 18px !important;
+    font-weight: 600;
+    text-transform: uppercase;
+  }
+}
+</style>
